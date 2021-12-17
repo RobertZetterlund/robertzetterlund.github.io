df81adbc9dede76114870e4cb28967bde2167b84
RobertZetterlund <robert.zetterlund@outlook.com>
2021-12-17 21:59:02 +0100

    theme and footer header

---
+++ src/ToggleTheme.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+import styled, {css} from 'styled-components'
+
+
+
+
+export const ToggleTheme = ({toggleTheme, theme}: {toggleTheme: () => void, theme:'light'|'dark'})=> {
+
+    return (
+        <Toggle.Container onClick={toggleTheme}>
+                <Toggle.Track>
+                    <Toggle.Icon>💡</Toggle.Icon>
+                    <Toggle.Icon>🦉</Toggle.Icon>
+                    <Ball $light={theme==='light'} />
+                </Toggle.Track>
+            </Toggle.Container>
+        )
+}
+
+
+
+
+
+
+const ballLeftCss = css`
+left:3px`
+const ballRightCss = css`
+left:37px`
+
+
+const Ball = styled.div<{$light:boolean}>`
+  width:25px;
+  height:25px;
+  border-radius:50%;
+  position:absolute;
+  background-color: white;
+  transition: .33s;
+  ${({$light}) => $light ? ballLeftCss : ballRightCss}
+`
+
+const Toggle = {
+  Container: styled.div`
+    position:relative;
+    &:hover ${Ball} {
+        box-shadow: 0 0 3px 2px green;
+    }
+    cursor:pointer;
+  `,
+  Icon: styled.div`
+    align-items: center;
+    display: flex;
+    height: 8px;
+    justify-content: center;
+    width: 8px;
+  `,
+  Track: styled.div`
+  border-radius:20px;
+  background-color:gray;
+  width:40px;
+  height:30px;
+  display:flex;
+    justify-content:space-between;
+    padding: 0 12px;
+    align-items:center;
+    `
+  
+}
+
+
---
+++ src/theme/theme.tsx
@@ -0,0 +1,18 @@
+// theme.ts
+
+import { DefaultTheme } from "styled-components";
+export const lightTheme: DefaultTheme = {
+  background: "#FFF6EE",
+  accent: "#454851",
+};
+
+export const darkTheme: DefaultTheme = {
+  background: "#454851",
+  accent: "#FFF6EE",
+};
+
+
+
+
+
+
